11d97fae25cc9fc6f3493efb858d614665f3e76d
inho <inho042@naver.com>
2023-05-17 18:06:20 +0900

    저녁먹기전 수업

---
+++ src/day01/TestClass02.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class TestClass02 {
+	public static void main(String[] args) {
+		/*
+		 escape 문자
+		 - 문자열 안에서 특수한 기능을 수행
+		 - \n : 엔터값과 비슷
+		 - \t : 커서를 탭 크기만큼 오른쪽으로 이동
+		 - \" : "(큰 따옴표) 하나 표현
+		  - \\ : \(역슬러시) 하나 표현
+		 */
+			System.out.println("안\n녕하세요");
+			System.out.println("안\t녕하세요");
+			
+			//;(세미콜론) : 마침표, 즉 문장의 마지막에는 꼭 들어간다
+	}
+}	
